2427d4637cdf78ca125aafff8ea4ac55d7908586
bpteo2006 <bpteo2006@gmail.com>
2024-05-26 00:19:54 +0800

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Shopping List
+        System.out.println("> Milk\n");
+        System.out.println("   > Semi-skimmed\n");
+        System.out.println("   > Medium\n");
+        System.out.println("> Eggs\n");
+        System.out.println("   > Free-range\n");
+        System.out.println("> Bread");
+    }
+}
